e8d6fbc74f81f3f237f1f684662a759e3f0fab03
Martijn Vogten <martijn@vogten.nl>
2013-06-10 21:08:29 +0300

    Fix issue #5 Code repetition

--- xml/dublincore.php
+++ xml/dublincore.php
@@ -18,8 +18,6 @@ class DublinCoreRecord {
     $record = $doc->createElementNS(NS::OAI_DC, "dc");
     $record->setAttributeNS(NS::XML, "xmlns:oai_dc", NS::OAI_DC);
     $record->setAttributeNS(NS::XML, "xmlns:dc", NS::DC);
-    $record->setAttributeNS(NS::XML, "xmlns:oai_dc", NS::OAI_DC);
-    $record->setAttributeNS(NS::XML, "xmlns:dc", NS::DC);
     $record->setAttributeNS(NS::XSI, "xsi:schemaLocation", "http://www.openarchives.org/OAI/2.0/oai_dc/ http://www.openarchives.org/OAI/2.0/oai_dc.xsd");
     
     foreach($this->fields as $f) {
